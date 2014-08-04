ce9be864c434d54ee8347cdc4414b3edec4a0e2a
Ovcharenko Volodymyr <ovcharenkovv@gmail.com>
2014-08-04 20:39:48 +0300

    create dropdown for car brand

--- src/main/java/ua/net/itlabs/beans/CreateDriverBean.java
+++ src/main/java/ua/net/itlabs/beans/CreateDriverBean.java
@@ -2,16 +2,28 @@ package ua.net.itlabs.beans;
 
 import ua.net.itlabs.model.Driver;
 
-//import javax.annotation.ManagedBean;
-//import javax.enterprise.context.SessionScoped;
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import java.util.HashMap;
+import java.util.Map;
 
 @ManagedBean(name = "createDriverBean")
 @SessionScoped
 public class CreateDriverBean {
 
     private Driver driver = new Driver();
+
+    private Map<String, String> brands = new HashMap<String, String>();
+
+    @PostConstruct
+    public void init() {
+        brands = new HashMap<String, String>();
+        brands.put("Audi", "Audi");
+        brands.put("BMW", "BMW");
+        brands.put("Opel", "Opel");
+    }
+
 
     public Driver getDriver() {
         return driver;
@@ -23,5 +35,13 @@ public class CreateDriverBean {
 
     public String create() {
         return "view";
+    }
+
+    public Map<String, String> getBrands() {
+        return brands;
+    }
+
+    public void setBrands(Map<String, String> brands) {
+        this.brands = brands;
     }
 }
